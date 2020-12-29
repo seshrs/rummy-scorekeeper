cee765ce4264450d697994d43a66059ce14648ea
Hiranmaya Gundu <hiranmayagundu@gmail.com>
2020-12-29 13:09:57 +0530

    set up express to correctly route

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,9 @@ const PORT = 3001;
 
 const clientDir = path.join(__dirname, '../../client/build');
 app.use(express.static(clientDir));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(clientDir, 'index.html'));
+});
 
 server.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
